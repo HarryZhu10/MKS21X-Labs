b07c70bca4c55db213e30db16385e238d67fc471
HarryZhu231227919 <hzhu30@stuy.edu>
2021-10-22 01:02:49 -0400

    Created more methods and made sure that they work for a. Now, I need to figure out what to do for b.

--- 01-Object/Kitty.java
+++ 01-Object/Kitty.java
@@ -1,22 +1,35 @@
 public class Kitty {
 
-  private int name;
+  private String name;
   private int age;
 
 
-  public Kitty(int startName, int startAge) {
+  public Kitty(String startName, int startAge) {
     name = startName;
     age = startAge;
   }
 
-  public int getName() {
+  public String getName() {
+    if (name == null) {
+      return "Mittens";
+    }
     return name;
   }
 
   public int getAge() {
+    if (name == null) {
+  return 2;
+  }
     return age;
   }
 
-  
+  public void makeOlder() {
+    age += 1;
+  }
+
+
+  public void changeName (String newName) {
+    name = newName;
+  }
 
 }
