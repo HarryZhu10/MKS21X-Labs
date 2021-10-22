9445d5693becb143f006eb4a6035bbf43135526f
HarryZhu231227919 <hzhu30@stuy.edu>
2021-10-22 00:33:38 -0400

    added some of the methods

--- 01-Object/Kitty.java
+++ 01-Object/Kitty.java
@@ -1,12 +1,20 @@
 public class Kitty {
 
-private int name;
-private int age;
+  private int name;
+  private int age;
 
 
   public Kitty(int startName, int startAge) {
     name = startName;
     age = startAge;
+  }
+
+  public int getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
   }
 
   
