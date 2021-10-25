afd922d647d4155e5f8f7fe0292925d7240c5160
HarryZhu231227919 <hzhu30@stuy.edu>
2021-10-24 21:36:10 -0400

    Added the difference of x method

---
+++ 01-Object/Point.java
@@ -0,0 +1,22 @@
+public class Point{
+  private double x,y;
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+  public Point(Point p){
+    x= p.x;
+    y= p.y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+
+public double xDiff(Point P1, Point P2) {
+  return P1.x - P2.x;
+}
+  
+}
