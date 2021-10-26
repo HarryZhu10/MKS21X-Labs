b88366123b931cab6e0f17640940bd25cf355976
HarryZhu231227919 <hzhu30@stuy.edu>
2021-10-25 21:35:26 -0400

    I added the basics, including one constructor and the fields.

---
+++ 02-Triangle/Triangle.java
@@ -0,0 +1,29 @@
+public class Triangle {
+
+private Point v1,v2,v3;
+
+public Triangle(Point P1, Point P2, Point P3) {
+  v1 = P1;
+  v2 = P2;
+  v3 = P3;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
