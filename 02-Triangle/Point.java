c98445edeeee38ba9503ab2cf7ba8fe093eeae07
HarryZhu231227919 <hzhu30@stuy.edu>
2021-10-26 14:50:59 -0400

    I completed part of the equals method

---
+++ 02-Triangle/Point.java
@@ -0,0 +1,40 @@
+public class Point{
+  private double x,y;
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+  public Point(Point p){
+    x= p.x;
+    y= p.y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+
+
+
+public static double distance(Point P1, Point P2) {
+return Math.sqrt((P1.getX() - P2.getX()) * ((P1.getX() - P2.getX())) + ((P1.getY() - P2.getY())) * ((P1.getY() - P2.getY())));
+
+}
+
+public double distanceTo(Point P2) {
+  return Math.sqrt((x - P2.getX()) * (x - P2.getX()) + (y - P2.getY()) * (y - P2.getY()));
+}
+
+
+public String toString() {
+  return "(" + x + ", " + y + ")";
+}
+
+public boolean equals(Point other) {
+  if (((x - other.x)/other.x) <= 0.00001) {
+    return
+  }
+}
+
+}
