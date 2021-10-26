0e1f00316be1d3dbe82162b667096a338d8718e3
HarryZhu231227919 <hzhu30@stuy.edu>
2021-10-25 21:10:39 -0400

    I added the test case for the toString method.

--- 01-Object/Demo.java
+++ 01-Object/Demo.java
@@ -7,13 +7,15 @@ public static double distance(Point P1, Point P2) {
 
 
 
+
   public static void main(String[]args){
     Point P1 = new Point(0,0);
     Point P2 = new Point(3,4);
 
     System.out.println(  distance(P1,P2)); //call1
-//    System.out.println(  Point.distance(P1,P2)); //call 2
-//    System.out.println(  P1.distanceTo(P2)); //call 3
+    System.out.println(  Point.distance(P1,P2)); //call 2
+    System.out.println(  P1.distanceTo(P2)); //call 3
+    System.out.println( P2.toString());
     //This one is IMPLIED strongly. You cannot tell by the calling style alone,
     //but you can make an educated guess.
   }
