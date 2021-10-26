ea2c11b2732e97d0ec84c3d78a18d41f66481497
HarryZhu231227919 <hzhu30@stuy.edu>
2021-10-25 23:13:57 -0400

    I added the getperimeter method

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -8,8 +8,14 @@ public Triangle(Point P1, Point P2, Point P3) {
   v3 = P3;
 }
 
+public Triangle(double Ax, double Ay, double Bx, double By, double Cx, double Cy) {
+
+}
 
 
+public double getPerimeter() {
+  return distanceTo(v1,v2) + distanceTo(v2, v3) + distanceTo(v3, v1);
+}
 
 
 
