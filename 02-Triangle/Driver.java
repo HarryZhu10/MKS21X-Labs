0d32c4e6a96f9b6ac218bed2224f8c117505a4e9
HarryZhu231227919 <hzhu30@stuy.edu>
2021-10-26 22:50:07 -0400

    I added all the test cases for each of the methods in Triangle.java.

--- 02-Triangle/Driver.java
+++ 02-Triangle/Driver.java
@@ -6,11 +6,15 @@ public class Driver {
     Point P1 = new Point (0,0);
     Point P2 = new Point (3,4);
     Point P3 = new Point (3,4);
+    Point P4 = new Point (3,0);
     double test0 = 0;
     double test1 = 0;
     double test2 = 5;
     double test3 = 6;
     double test4 = 6.00005;
+    Triangle T1 = new Triangle(P1,P2,P4);
+    Triangle T2 = new Triangle (P1, new Point (1,0), new Point (1,1));
+    Triangle T3 = new Triangle (P1, new Point (4,0), new Point (2,2 * Math.sqrt(3)));
 
     System.out.println(P1.getX());
     System.out.println(P1.getY());
@@ -23,11 +27,21 @@ public class Driver {
     System.out.println(Point.closeEnough(test4,test3));
     System.out.println(P2.equals(P1));
     System.out.println(P2.equals(P3));
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println();
+    System.out.println(T1.getPerimeter());
+    System.out.println(T1.getVertex(2));
+    System.out.println(T1.getVertex(3));
+    T1.setVertex(1, new Point (2,2));
+    System.out.println(T1.getVertex(1));
+    System.out.println(T1.toString());
+    Triangle copyT1 = T1;
+    System.out.println(T1.equals(copyT1));
+    System.out.println(T1.equals(T2));
+    T1 = new Triangle(P1,P2,P4);
+    System.out.println(T1.classify());
+    System.out.println(T2.classify());
+    System.out.println(T3.classify());
+    System.out.println(T1.area());
+
 
 
   }
