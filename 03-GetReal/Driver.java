2f947c6530e5f98cd4b77c831606ae446800535c
HarryZhu231227919 <hzhu30@stuy.edu>
2021-11-01 23:05:19 -0400

    created test cases for add method from RealNumber.java

---
+++ 03-GetReal/Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+
+public static void main(String[] args) {
+  RealNumber test1 = new RealNumber(6.0);
+  RealNumber test2 = new RealNumber(2.0);
+
+  System.out.println(test1.add(test2));
+}
+
+
+
+}
