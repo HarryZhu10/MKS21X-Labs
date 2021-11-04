a8e6dffb33f2da9890d22a2d5a2f2520135820ab
HarryZhu231227919 <hzhu30@stuy.edu>
2021-11-03 22:37:46 -0400

    I added the equals and compareTo method

--- 03-GetReal/RealNumber.java
+++ 03-GetReal/RealNumber.java
@@ -46,4 +46,21 @@ public class RealNumber{
   public String toString(){
     return ""+value;
   }
+
+public int compareTo(RealNumber other) {
+  if (value > other.value) {
+    return 1;
+  } else if (value < other.value) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+public boolean equals(RealNumber other) {
+  if (getValue() == other.getValue()) {
+    return true;
+  }
+  return false;
+}
 }
