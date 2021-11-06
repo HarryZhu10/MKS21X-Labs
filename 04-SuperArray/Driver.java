e721d8ab3f762cd51ebbdd32442150743d61d2bf
HarryZhu231227919 <hzhu30@stuy.edu>
2021-11-06 18:13:37 -0400

    I added the basic test cases.

---
+++ 04-SuperArray/Driver.java
@@ -0,0 +1,23 @@
+
+public class Driver {
+  public static void main(String args[]){
+    SuperArray SA = new SuperArray();
+    alist.add("Fish");
+    //displaying elements
+    System.out.println(SA);
+    System.out.println(SA.toStringDebug());
+
+    alist.add("Cat");
+    //displaying elements
+    System.out.println(SA);
+    System.out.println(SA.toStringDebug());
+
+    alist.add("Dog");
+    alist.add("Pig");
+    alist.add("Cow");
+    //displaying elements
+    System.out.println(SA);
+    System.out.println(SA.toStringDebug());
+
+  }
+}
