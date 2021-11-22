83b1883b3e583ee8e0883919ea8b5d91d241423c
HarryZhu231227919 <hzhu30@stuy.edu>
2021-11-22 14:48:59 -0500

    Starting

---
+++ 06-Advent/Triangles.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.File;
+public class Triangles {
+
+//Part 1
+public static void validTriangleNum() {}
+File file = new File("Test.txt");
+Scanner check = new Scanner(file);
+
+while (hasNextInt() == true) {
+
+}
+
+}
