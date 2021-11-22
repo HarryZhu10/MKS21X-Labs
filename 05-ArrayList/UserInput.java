2e3858feea1e088d09e0dd24c9e098d5ecd6676c
HarryZhu231227919 <hzhu30@stuy.edu>
2021-11-21 22:14:32 -0500

    Added the basics

---
+++ 05-ArrayList/UserInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class UserInput {
+  public static void main (String[] args) {
+    ArrayList<Double>container = new ArrayList<Double>();
+    Scanner negative = new Scanner(System.in);
+    System.out.println("Enter a non negative number. A negative number will end the program:")
+    while (nextDouble() > 0) {
+
+    }
+  }
+}
