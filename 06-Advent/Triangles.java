3bcc01da60d1695d56ce5f4b70a582de285f17b3
HarryZhu231227919 <hzhu30@stuy.edu>
2021-11-23 14:50:33 -0500

    Minor additions

--- 06-Advent/Triangles.java
+++ 06-Advent/Triangles.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 import java.io.File;
+import java.util.Arrays;
+import java.io.FileNotFoundException;
 public class Triangles {
 
-//Part 1
-public static void validTriangleNum() {}
-File file = new File("Test.txt");
-Scanner check = new Scanner(file);
 
-while (hasNextInt() == true) {
+public static void main(String[] args) {
+  try {
+    File file = new File("Test.txt");
+    Scanner check = new Scanner(file);
+    
+    while (check.hasNextInt() == true) {
+      System.out.println("testing");
+    }
 
+
+
+
+
+} catch (FileNotFoundException ex) {
+         System.out.println("File not found");
+     }
 }
 
 }
