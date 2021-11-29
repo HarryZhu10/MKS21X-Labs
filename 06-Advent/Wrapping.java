31f7a989be9e5f3c6d75a3de4369aa6bfe11254c
HarryZhu231227919 <hzhu30@stuy.edu>
2021-11-28 20:16:27 -0500

    Part 1 code completed

--- 06-Advent/Wrapping.java
+++ 06-Advent/Wrapping.java
@@ -2,13 +2,34 @@
 public class Wrapping {
   public static void main(String[] args) {
     File argsFile = new File(args[0]);
-
+    int totalAmount = 0;
     try {
       Scanner in = new Scanner(argsFile);
+      while (in.hasNextInt()) {
+        int length = in.nextInt();
+        in.nextChar();
+        int width = in.nexInt();
+        in.nextChar();
+        int height = in.nextInt();
+        int minArea = length * width;
 
+        if (length * height < minArea) {
+          minArea = length * height;
+        }
+
+        if (width * height < minArea) {
+          minArea = width * height;
+        }
+
+        totalAmount = totalAmount + 2 * width * height + 2 * length * height + 2 * width * length + minArea;
+      }
+      in.close();
     } catch (FileNotFoundException ex) {
 
     }
+    System.out.println("" + totalAmount);
+  }
+
 
 
 
