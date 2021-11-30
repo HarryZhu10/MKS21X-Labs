7a7bf7e5f013e4474383d47f86bee2328b6d9bae
HarryZhu231227919 <hzhu30@stuy.edu>
2021-11-30 14:27:17 -0500

    Starting

---
+++ 07-BinarySearch/BinarySearch.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class BinarySearch {
+  public static int binarySearch(int[]data,int target){
+  int[] sortedData = new Arrays.sort(data);
+
+  return sortedData;
+
+
+  }
+
+  public static void main (String[] args) {
+    int[] data = {2,3,5,1,2,22};
+    int[] test = new Arrays.sort(data);
+    System.out.println(Arrays.toString(test));
+  }
+
+}
