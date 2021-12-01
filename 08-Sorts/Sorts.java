2ab9911e656ba2a26d5e54db70c7835b19a6cb5c
HarryZhu231227919 <hzhu30@stuy.edu>
2021-12-01 13:58:58 -0500

    Test case

---
+++ 08-Sorts/Sorts.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class Sorts {
+  public static void main (String[] args) {
+    int[] messyList = {3,2,1,5,5,0};
+    bubbleSort(messyList);
+    int[] sortedList = {0,1,2,3,5,5};
+    System.out.println(Arrays.toString(messyList) + " should be the same as " + Arrays.toString(sortedList));
+  }
+
+
+  public static void bubbleSort(int[] data) {
+
+  }
+}
