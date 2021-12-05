2eed2556d8e10f9032e32a90cc484b931f8fe018
HarryZhu231227919 <hzhu30@stuy.edu>
2021-12-05 15:43:22 -0500

    Finished Binary Search method

--- 07-BinarySearch/BinarySearch.java
+++ 07-BinarySearch/BinarySearch.java
@@ -1,16 +1,33 @@
 import java.util.Arrays;
 public class BinarySearch {
-  public static int binarySearch(int[]data,int target){
-  int[] sortedData = new Arrays.sort(data);
+  public static int binarySearch(int[] data,int target){
+  int[] sortedData = data;
+  Arrays.sort(sortedData);
+  int maxIndex = data.length - 1;
+  int minIndex = 0;
+  int middle = (maxIndex + minIndex) / 2;
 
-  return sortedData;
-
+  while (min <= max) {
+    if (target == data[middle]) {
+      return middle;
+    }
+    if (target > data[middle]) {
+      minIndex = middle + 1;
+      middle = (maxIndex + minIndex) / 2;
+    }
+    if (target < data[middle]) {
+      maxIndex = middle - 1;
+      middle = (maxIndex + minIndex) / 2;
+    }
+  }
+  return -1;
 
   }
 
   public static void main (String[] args) {
     int[] data = {2,3,5,1,2,22};
-    int[] test = new Arrays.sort(data);
+    int[] test = data;
+    Arrays.toString(test);
     System.out.println(Arrays.toString(test));
   }
 
