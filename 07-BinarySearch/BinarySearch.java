563b6ca1a52afdd7aca265a74be04ad3a2a3db80
HarryZhu231227919 <hzhu30@stuy.edu>
2021-12-05 15:52:40 -0500

    Test cases

--- 07-BinarySearch/BinarySearch.java
+++ 07-BinarySearch/BinarySearch.java
@@ -25,8 +25,24 @@ public class BinarySearch {
   }
 
   public static void main (String[] args) {
+    int[] 
     int[] data = {2,3,5,1,2,22};
     System.out.println(binarySearch(data, 3));
+    System.out.println(binarySearch(data, 7));
+    System.out.println(binarySearch(data, 2));
+    System.out.println(binarySearch(data, 22));
+    int result;
+result = binarySearch(new int[]{0,0,0,0,0,0,0},0);
+System.out.println(result);//should be 3
+result = binarySearch(new int[]{0,0,0,0,0,0,0},1);
+System.out.println(result);//should be -1
+
+result = binarySearch(new int[]{0,1,2,3,4,5,6},0);
+System.out.println(result);//should be 0
+//...
+result = binarySearch(new int[]{0,1,2,3,4,5,6},6);
+System.out.println(result);//should be 6
+//Tip: You can use a loop to find if it works on 0,1,2,3,4,5,and 6 in this array!
   }
 
 }
