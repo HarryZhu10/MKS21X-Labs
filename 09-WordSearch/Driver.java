0a4e040b6c1b4cddfe029607e3934dbc82d38d8e
HarryZhu231227919 <hzhu30@stuy.edu>
2021-12-12 21:51:05 -0500

    Added a test case for addWordDiagonal method

--- 09-WordSearch/Driver.java
+++ 09-WordSearch/Driver.java
@@ -8,5 +8,7 @@ puzzle.addWordHorizontal("MATH", 2, 1);
 System.out.println(puzzle);
 puzzle.addWordVertical("MAAE", 0, 2);
 System.out.println(puzzle);
+puzzle.addWordDiagonal("MTAA", 0, 0);
+System.out.println(puzzle);
   }
 }
