e42285bb518f498c4da68f1386bca34544307453
HarryZhu231227919 <hzhu30@stuy.edu>
2021-12-12 19:57:29 -0500

    Added a test case for each method

--- 09-WordSearch/Driver.java
+++ 09-WordSearch/Driver.java
@@ -1,7 +1,12 @@
 public class Driver {
   public static void main (String[] args) {
-    WordSearch puzzle = new WordSearch(2,5);
+    WordSearch puzzle = new WordSearch(4,5);
 
+System.out.println(puzzle);
+
+puzzle.addWordHorizontal("MATH", 2, 1);
+System.out.println(puzzle);
+puzzle.addWordVertical("MAAE", 0, 2);
 System.out.println(puzzle);
   }
 }
