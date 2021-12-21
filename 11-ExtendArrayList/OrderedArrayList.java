989dcb62a5e094501f00e8f6947b4253096def72
HarryZhu231227919 <hzhu30@stuy.edu>
2021-12-21 13:43:31 -0500

    I did some of the methods.

--- 11-ExtendArrayList/OrderedArrayList.java
+++ 11-ExtendArrayList/OrderedArrayList.java
@@ -15,6 +15,9 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
         when inserting into the OrderedArrayList .*/
 
         //Ask if we need to consider that the list may have a length of 2
+        if (value == null) {
+          return 0;
+        }
 
         for (int i = size() - 1; i >= 0; i --) {
           if (i != 0 && i != size() - 1) {
@@ -31,9 +34,16 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
           }
         }
         }
-
+        return -1;
       }
 
+      public boolean add(T element) {
+        super.add(whereToPlace(element), element);
+        return true;
+      }
 
-
+      public void add(int index, T element) {
+        add(element);
+      }
+      public T set()
 }
