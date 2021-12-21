69926337ea633448e577fc686abf06089c46110a
HarryZhu231227919 <hzhu30@stuy.edu>
2021-12-20 23:52:44 -0500

    I built the WhereToPlace helper method. Still need a return thing.

---
+++ 11-ExtendArrayList/OrderedArrayList.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+
+  public OrderedArrayList () {
+    super();
+  }
+
+  public OrderedArrayList (int startingCapacity) {
+    super(startingCapacity);
+  }
+
+
+  private int whereToPlace(T value){
+        /*return the index that the value should be placed
+        when inserting into the OrderedArrayList .*/
+
+        //Ask if we need to consider that the list may have a length of 2
+
+        for (int i = size() - 1; i >= 0; i --) {
+          if (i != 0 && i != size() - 1) {
+          if (value.compareTo(get(i - 1)) >= 0 && value.compareTo(get(i + 1)) <= 0) {
+            return i;
+          }
+        } else if (i == size() - 1) {
+          if (value.compareTo(get(i)) >= 0) {
+            return i;
+          }
+        } else if (i == 0) {
+          if (value.compareTo(get(i)) <= 0) {
+            return i;
+          }
+        }
+        }
+
+      }
+
+
+
+}
