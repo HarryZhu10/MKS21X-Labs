e1e4244734e9b8a83a63d4260c87679062e4652c
HarryZhu231227919 <hzhu30@stuy.edu>
2022-01-02 22:10:40 -0500

    I added a variable that would track the powers of ten I needed to multiply by.

--- ClassWork/ToInt.java
+++ ClassWork/ToInt.java
@@ -6,13 +6,15 @@ public class ToInt {
   public static int stringToInt(String s){
        //return the value of Integer.parseInt(s) but do it yourself!
        //Do not use any built in parse methods.
-       int start = 0;
+       int end = 0;
        int result = 0;
+      int power = 0;
        if (s.charAt(0) == '-') {
-         start ++;
+         end ++;
        }
-       for (int i = start; i < s.length(); i ++) {
-         result = result + math.pow(10, i) * valueOfDigit(s.charAt(i));
+       for (int i = s.length() - 1; i >= end; i --) {
+         result = result + (int) Math.pow(10, power) * valueOfDigit(s.charAt(i));
+         power ++;
        }
 
        if (s.charAt(0) == '-') {
