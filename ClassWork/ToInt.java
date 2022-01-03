d782b247b81c7e65fbb2d5e067b9eeab29dc7ab9
HarryZhu231227919 <hzhu30@stuy.edu>
2022-01-02 21:48:40 -0500

    I'm going to create a for loop to go through each digit of the string and add them together. I added a case at the end to check if the number is negative or not.

--- ClassWork/ToInt.java
+++ ClassWork/ToInt.java
@@ -3,5 +3,16 @@ public class ToInt {
   public static int valueOfDigit(char c) {
     return c - 48;
   }
+  public static int stringToInt(String s){
+       //return the value of Integer.parseInt(s) but do it yourself!
+       //Do not use any built in parse methods.
 
+       for (int i = s.length() - 1; i >= 0; i --) {
+
+       }
+
+       if (s.substring(0,1) == "-") {
+         result = result * -1;
+       }
+     }
 }
