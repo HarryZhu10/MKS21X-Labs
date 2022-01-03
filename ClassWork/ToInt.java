ff5b71d367aafe63d995d6dfe63d30d22aafd51b
HarryZhu231227919 <hzhu30@stuy.edu>
2022-01-02 21:58:01 -0500

    I changed the loop so that it would start from the beginning. The loop goes through each digit and multiplies it by the appropriate power of 10 so that it would result in the int form of the string.

--- ClassWork/ToInt.java
+++ ClassWork/ToInt.java
@@ -6,13 +6,18 @@ public class ToInt {
   public static int stringToInt(String s){
        //return the value of Integer.parseInt(s) but do it yourself!
        //Do not use any built in parse methods.
-
-       for (int i = s.length() - 1; i >= 0; i --) {
-
+       int start = 0;
+       int result = 0;
+       if (s.charAt(0) == '-') {
+         start ++;
+       }
+       for (int i = start; i < s.length(); i ++) {
+         result = result + math.pow(10, i) * valueOfDigit(s.charAt(i));
        }
 
-       if (s.substring(0,1) == "-") {
+       if (s.charAt(0) == '-') {
          result = result * -1;
        }
+       return result;
      }
 }
