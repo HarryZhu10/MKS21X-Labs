e1d8e69e6d634511cce2462da9967d04f541a643
HarryZ <hzhu30@stuy.edu>
2022-01-09 18:19:00 -0500

    I added both the attack and specialAttack method. I made the special attack of the wizard, heal him and do a random amount of attack.

--- 12-Stuyablo/Wizard.java
+++ 12-Stuyablo/Wizard.java
@@ -11,13 +11,32 @@ public Wizard(String name) {
 }
 
 public Wizard(String name, String catchPhrase, int staffSize) {
-
+  super(name, 20 + (int) (Math.random() * 10));
+  setCatchPhrase(catchPhrase);
+  setStaffSize(staffSize);
 }
 
-
-public void applyDamage() {
-
+public void attack(Damageable other){
+  int damage = (int) (Math.random() * 10);
+  other.applyDamage(damage);
+  setStaffSize(getStaffSize() + 1);
+  System.out.println("The mythical " + this + " attacked " + other + " for " + damage + " damage!");
 }
+
+public void specialAttack(Damageable other) {
+  if (getStaffSize() >= 10) {
+    int heal = (int) (Math.random() * 10) + 1;
+    setHP(getHP() + heal);
+    int damage = (int) (Math.random() * 20);
+    other.applyDamage(damage);
+    System.out.println(this " has healed itself for " + heal " HP " + " and attacked for " + damage " damage!");
+    setStaffSize(getStaffSize() - 10);
+  } else {
+    System.out.println("Your staff is too small!")
+    attack(other);
+  }
+}
+
 
 public void setCatchPhrase (String catchPhrase) {
   this.catchPhrase = catchPhrase;
@@ -30,5 +49,6 @@ public int getStaffSize () {
 public void setStaffSize (int staffSize) {
   this.staffSize = staffSize;
 }
+
 
 }
