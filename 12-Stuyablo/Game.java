ad296395806e99694403cef3b2f7dfa9f84f6843
HarryZ <hzhu30@stuy.edu>
2022-01-09 18:33:37 -0500

    I added the Wizard into the fight with the two Warriors. I made sure that the Wizard's health and attack were functioning correctly. I removed one of the warrior's attack on the wizard in the second round so that I can see the healing mechanic behind the wizard's special attack.

--- 12-Stuyablo/Game.java
+++ 12-Stuyablo/Game.java
@@ -2,16 +2,22 @@ public class Game{
   public static void main(String[] args) {
     Adventurer a = new Warrior("Conan","Aaaaaaaar!",20);
     Adventurer b = new Warrior("Homer","Donuts?",0);
+    Adventurer c = new Wizard("Gandalf","Don't underestimate me!", 10);
+
     System.out.println(a+" ("+a.getHP()+"HP)");
     System.out.println(b+" ("+b.getHP()+"HP)");
+    System.out.println(c +" ("+c.getHP()+"HP)");
+    c.attack(a);
     a.attack(b);
-    b.attack(a);
+    b.attack(c);
     System.out.println(a+" ("+a.getHP()+"HP)");
     System.out.println(b+" ("+b.getHP()+"HP)");
+    System.out.println(c + " ("+c.getHP()+"HP)");
+    c.specialAttack(a);
     a.specialAttack(b);
-    b.specialAttack(a);
     System.out.println(a+" ("+a.getHP()+"HP)");
     System.out.println(b+" ("+b.getHP()+"HP)");
+    System.out.println(c + " ("+c.getHP()+"HP)");
 
     }
 
