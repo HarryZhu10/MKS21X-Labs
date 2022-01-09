659795f10a1910eb06f168ef467a9adc99ca2f5c
HarryZ <hzhu30@stuy.edu>
2022-01-09 17:23:53 -0500

    I am working on the constructors for the Wizard class and have completed some of the basic methods like getStaffSize and setStaffSize.

--- 12-Stuyablo/Wizard.java
+++ 12-Stuyablo/Wizard.java
@@ -1,4 +1,34 @@
 public class Wizard extends Adventurer {
-private 
+private int staffSize;
+private String catchPhrase;
+
+pubic Wizard() {
+  this("Gandalf");
+}
+
+public Wizard(String name) {
+  this(name, "You shall not pass!", 10);
+}
+
+public Wizard(String name, String catchPhrase, int staffSize) {
+
+}
+
+
+public void applyDamage() {
+
+}
+
+public void setCatchPhrase (String catchPhrase) {
+  this.catchPhrase = catchPhrase;
+}
+
+public int getStaffSize () {
+  return staffSize;
+}
+
+public void setStaffSize (int staffSize) {
+  this.staffSize = staffSize;
+}
 
 }
