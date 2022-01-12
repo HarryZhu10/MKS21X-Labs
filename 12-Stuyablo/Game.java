fadc1b55453eafb23635d7546ae399efb35c32b4
HarryZ <hzhu30@stuy.edu>
2022-01-12 12:17:54 -0500

    I added the code to create a 80 by 30 yellow border. Then, I printed

--- 12-Stuyablo/Game.java
+++ 12-Stuyablo/Game.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class Game{
   public static void main(String[] args) {
+    Text.hideCursor();
+    Text.clear();
+
+    for (int i = 1; i < 81; i ++) {
+      Text.go(1,i);
+      System.out.print(Text.colorize(" ", Text.YELLOW + Text.BACKGROUND));
+      Text.go(30,i);
+      System.out.print(Text.colorize(" ", Text.YELLOW + Text.BACKGROUND));
+    }
+
+    for (int i = 1; i < 31; i ++) {
+      Text.go(i,1);
+      System.out.print(Text.colorize(" ", Text.YELLOW + Text.BACKGROUND));
+      Text.go(i, 80);
+      System.out.print(Text.colorize(" ", Text.YELLOW + Text.BACKGROUND));
+    }
+
+    Text.go(31,1);
+    System.out.print(Text.colorize(">", Text.WHITE));
+    Text.showCursor();
+
     Adventurer a = new Warrior("Conan","Aaaaaaaar!",20);
     Adventurer b = new Warrior("Homer","Donuts?",0);
     Adventurer c = new Wizard("Gandalf","Don't underestimate me!", 10);
